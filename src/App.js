41a5bcbd3361fc8f4fcd5a0264c0cbc4c6c755e8
Artem <artemklinkov@yandex.ru>
2021-05-10 22:46:47 +0300

    CR fixes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
   return (
     <>
       <Card 
-        cardTitle="Card title"
-        cardText="Some quick example text to build on the card title and make up the bulk of the card's content."
+        imgURL={logo}
       >
-        <img src={logo} class="card-img-top" alt="..."/>
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>        
       </Card>
-      <Card 
-        cardTitle="Special title treatment"
-        cardText="With supporting text below as a natural lead-in to additional content."
-      />
+      <Card>
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>        
+      </Card>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
 function Card(props) {
   return(
     <div className="card" style={{width: '18rem'}}>
-      {props.children}
+      <img src={props.imgURL} alt="" />
       <div className="card-body">
-        <h5 className="card-title">{props.cardTitle}</h5>
-        <p className="card-text">{props.cardText}</p>
+        {props.children}
         <button className="btn btn-primary">Go somewhere</button>
       </div>
     </div>
